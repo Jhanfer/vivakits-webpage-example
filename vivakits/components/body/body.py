13fd99313c181c3ef62b6a4af407e7a94c3adcfe
Jhanfer <jhafer73@gmail.com>
2024-05-26 22:58:14 +0200

    Carousel Test

--- vivakits/components/body/body.py
+++ vivakits/components/body/body.py
@@ -8,11 +8,29 @@ from vivakits.components.body.body_component import images_badges_left,images_ba
 from reflex_motion import motion
 from vivakits.components.header.header import header
 from vivakits.components.body.body_anoucement import anoucement,text_area
+from reflex_responsive_carousel import responsive_carousel
 
 def body() -> rx.Component:
     return rx.vstack(
                 header(),
                 cr.body_carousel("50em","20em","9"),
+                responsive_carousel(
+                        header_image_content("Zapato","x talla",im.ART_1),
+                        header_image_content("Zapato","x talla",im.ART_2),
+                        header_image_content("Zapato","x talla",im.ART_3),
+                        header_image_content("Zapato","x talla",im.ART_4),
+                        header_image_content("Zapato","x talla",im.ART_5),
+                        header_image_content("Zapato","x talla",im.ART_1),
+                        header_image_content("Zapato","x talla",im.ART_2),
+                        header_image_content("Zapato","x talla",im.ART_3),
+                        header_image_content("Zapato","x talla",im.ART_4),
+                        header_image_content("Zapato","x talla",im.ART_5),
+                    width="25em",
+                    infinite_loop=True,
+                    auto_play=True,
+                    swipeable=True,
+                    height="50"
+                ),
                 images_badges_left(title_1="LOS MEJORES ESTILOS",src=im.BADGE_1,wi=70,ini=0,padding="0em",padding_heading="15em"),
                 images_badges_right(title_1="SIENTE EL DEPORTE",src=im.BADGE_2,x_f=720,x_i=800,padding="0em",padding_heading="10.2em"),
                 rx.flex(
